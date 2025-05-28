5bc355215f09f8a33c4aaa3b7c2d352f58a800fd
Dhaval <dp9718776@gmail.com>
2025-05-28 10:22:16 +0530

    Create Calculator_example.cpp

---
+++ Calculator_example.cpp
@@ -0,0 +1,50 @@
+// Very simplest calculator
+#include <rlImGui.h>
+#include <imgui.h>
+
+int main()
+{
+    InitWindow(800, 600, "raylib + ImGui Example");
+
+    SetTargetFPS(60);
+    rlImGuiSetup(true);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+
+        rlImGuiBegin();
+
+        static double num1 = 0;
+        static double num2 = 0;
+        static double result = 0;
+
+        ImGui::Begin("Calculator");
+
+        ImGui::InputDouble("Number 1", &num1);
+        ImGui::InputDouble("Number 2", &num2);
+
+        if (ImGui::Button("Add"))
+            result = num1 + num2;
+
+        if (ImGui::Button("Substract"))
+            result = num1 - num2;
+
+        if (ImGui::Button("Divide"))
+            result = num1 / num2;
+
+        if (ImGui::Button("Multiply"))
+            result = num1 * num2;
+
+        ImGui::Text("Result: %f", result);
+
+        ImGui::End();
+
+        rlImGuiEnd();
+        EndDrawing();
+    }
+
+    rlImGuiShutdown();
+    CloseWindow();
+    return 0;
+}
